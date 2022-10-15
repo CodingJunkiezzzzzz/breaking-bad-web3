c117e7490090b1c9768491345cc09b215d5ce5f7
Umut Aktaş <umut@iqmotion.com>
2022-10-15 14:06:27 +0300

    added metamask login

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,23 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { SSRProvider } from "react-bootstrap";
+import type { AppProps } from "next/app";
+import { ThirdwebWeb3Provider } from "@3rdweb/hooks";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const connectors = {
+    injected: {},
+  };
+  return (
+    <ThirdwebWeb3Provider
+      connectors={connectors}
+      supportedChainIds={[1, 80001]}
+    >
+      <SSRProvider>
+        <Component {...pageProps} />
+      </SSRProvider>
+    </ThirdwebWeb3Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,80 @@
 import type { NextPage } from "next";
-import type { Character } from "../types/character";
+import type { Character as TypeCharacter } from "../types/character";
+import Character from "../components/Charater";
+import { Row, Col, Container, Button } from "react-bootstrap";
+import { useWeb3 } from "@3rdweb/hooks";
+import { useEffect } from "react";
+import Swal from "sweetalert2";
 
 type Props = {
-  data: Character[];
+  data: TypeCharacter[];
 };
 
 const Home: NextPage<Props> = ({ data }) => {
+  const { connectWallet, address, error, disconnectWallet } = useWeb3();
+  useEffect(() => {
+    if (error) {
+      Swal.fire({
+        icon: "error",
+        title: error.name,
+        text: error.message,
+      });
+    }
+  }, [error]);
+
   return (
-    <div>
-      {data.map((character) => (
-        <div key={character.char_id}>{character.name}</div>
-      ))}
-    </div>
+    <Container>
+      <div className="d-flex justify-content-between py-4">
+        <h1>Breaking Bad Characters</h1>
+        {!address ? (
+          <Button
+            onClick={() => connectWallet("injected")}
+            variant="dark"
+            className="d-flex align-items-center gap-2"
+          >
+            <img
+              alt="Metamask Logo"
+              src="/metamask.svg"
+              width={32}
+              height={32}
+            ></img>
+            <span>Login with MetaMask</span>
+          </Button>
+        ) : (
+          <div className="d-flex flex-column justify-content-end align-items-end">
+            <strong>Logged in ETH address</strong>
+            <span>{address}</span>
+            <Button
+              onClick={disconnectWallet}
+              variant="dark"
+              className="d-flex align-items-center gap-2"
+            >
+              <span>Reconnect</span>
+            </Button>
+          </div>
+        )}
+      </div>
+      <Row>
+        {data.map((character) => (
+          <Col
+            style={{ marginBottom: "30px" }}
+            xs={12}
+            md={6}
+            xl={4}
+            xxl={3}
+            key={character.char_id}
+          >
+            <Character style={{ height: "100%" }} character={character} />
+          </Col>
+        ))}
+      </Row>
+    </Container>
   );
 };
 
 export async function getServerSideProps() {
   const res = await fetch("https://www.breakingbadapi.com/api/characters");
-  const data: Character[] = await res.json();
+  const data: TypeCharacter[] = await res.json();
 
   return {
     props: {
